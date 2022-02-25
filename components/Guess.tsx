c525cca62859115e6622ea3e68192fd6160648d8
Daniel Bark <danba340@gmail.com>
2022-02-25 14:13:02 +0100

    double letter in guess, single in answer, dont show yellow on both fix

--- components/Guess.tsx
+++ components/Guess.tsx
@@ -7,16 +7,24 @@ export default function Guess({ guess, word }: { guess: string, word: string }) 
 			{guess.split('').map((letter, index) => {
 				const isCorrect = word[index] === letter.toLowerCase();
 				let letterCount = 0;
+				let letterCountGuess = 0;
+				let letterCountBeforeIndex = 0;
 				let corrects = 0;
 				for (let i = 0; i < 5; i++) {
 					if (word[i] === letter.toLowerCase()) {
 						letterCount++
 					}
+					if (guess[i] === letter) {
+						letterCountGuess++
+						if (i < index) {
+							letterCountBeforeIndex++;
+						}
+					}
 					if (word[i] === guess[i].toLowerCase() && guess[i] === letter) {
 						corrects++;
 					}
 				}
-				const shouldShowExists = letterCount > corrects;
+				const shouldShowExists = letterCount > corrects && letterCountBeforeIndex < letterCount;
 
 				return (<div key={index} className={`letter-input flex justify-center align-center ${isCorrect && "bg-green"} ${!isCorrect && shouldShowExists ? "bg-yellow c-black" : ""}`}>
 					<Input readOnly disableUnderline={true} value={letter} sx={{ maxWidth: "3ch", color: "#eee", textTransform: "uppercase" }} inputProps={{ maxLength: 1 }} />
